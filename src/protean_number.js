9f53c451cdd4892c39cc2804c30e1de063e8dec9
Joel Byler <joelbyler@gmail.com>
2013-02-25 14:01:26 -0500

    Number even and odd functions

--- src/protean_number.js
+++ src/protean_number.js
@@ -3,3 +3,11 @@ Number.prototype.next = function(){
     return this+1;
 };
 
+Number.prototype.odd = function(){
+    return (this % 2 == 1);
+};
+
+Number.prototype.even = function(){
+    return (this % 2 == 0);
+};
+
--- test/number_spec.js
+++ test/number_spec.js
@@ -3,8 +3,16 @@ var Person = require(__dirname + '/../src/protean_number');
 
 describe('Integer Extension', function() {
   it('Knows what\'s next', function() {
-    1.0.next().should.equal(2);
+    (1.0).next().should.equal(2);
     (-1).next().should.equal(0);
+  });
+  it('Knows when it is odd', function() {
+    (1.0).odd().should.equal(true);
+    (2.0).odd().should.equal(false);
+  });
+  it('Knows when it is even', function() {
+    (1.0).even().should.equal(false);
+    (2.0).even().should.equal(true);
   });
 });
 
